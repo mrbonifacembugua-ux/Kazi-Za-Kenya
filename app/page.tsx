8ffd723410f6f87038fc5b5d689e647c9fd7b713
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-24 13:01:29 +0300

    restore temporary

--- app/page.tsx
+++ app/page.tsx
@@ -4,4 +4,6 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "../lib/supabase";
 
-// Restore must be done from the prior commit.
+export default function Home() {
+  return <div />;
+}
