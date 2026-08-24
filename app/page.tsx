13b35cfc542cce1b2220855fe2297e182816f8e4
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-24 13:01:37 +0300

    restore

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,1 @@
-"use client";
-
-import { useEffect, useMemo, useRef, useState } from "react";
-import { useRouter } from "next/navigation";
-import { supabase } from "../lib/supabase";
-
-export default function Home() {
-  return <div />;
-}
+RESTORE_ME
