7b309ba80d2629922f8b54c51002ebde50738219
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-24 13:01:00 +0300

    temporary

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-PLACEHOLDER
+"use client";
+
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../lib/supabase";
+
+export default function Home() {
+  return <div />;
+}
