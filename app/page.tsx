5b2becae3ce15238ca63d8a80530a673d50e5527
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-24 13:01:14 +0300

    mark restore point

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,4 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "../lib/supabase";
 
-export default function Home() {
-  return <div />;
-}
+// Restore must be done from the prior commit.
