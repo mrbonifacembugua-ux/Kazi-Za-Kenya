ab52b1b9de5d2fdeed2637c781a888a90fef384e
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-30 14:15:44 +0300

    Show account access in mobile marketplace header

--- app/template.tsx
+++ app/template.tsx
@@ -1,10 +1,12 @@
 import JobCardProfilePhotos from "./JobCardProfilePhotos";
+import MobileMarketplaceHeader from "./MobileMarketplaceHeader";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <JobCardProfilePhotos />
+      <MobileMarketplaceHeader />
     </>
   );
 }
