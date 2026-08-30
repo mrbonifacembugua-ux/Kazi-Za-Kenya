5c72922b402d848f0a0b313ccaea6c45949fd4c1
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-08-30 14:46:40 +0300

    Mount account typography hierarchy

--- app/template.tsx
+++ app/template.tsx
@@ -1,6 +1,7 @@
 import JobCardProfilePhotos from "./JobCardProfilePhotos";
 import MobileMarketplaceHeader from "./MobileMarketplaceHeader";
 import MobileLoginAuthFix from "./MobileLoginAuthFix";
+import AccountTypographyPolish from "./AccountTypographyPolish";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <JobCardProfilePhotos />
       <MobileMarketplaceHeader />
       <MobileLoginAuthFix />
+      <AccountTypographyPolish />
     </>
   );
 }
