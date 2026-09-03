3e89ee8646b4c9861f39a87654587047deec5759
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-09-03 19:11:18 +0300

    Temporarily isolate base demo content to diagnose marketplace freeze

--- app/template.tsx
+++ app/template.tsx
@@ -38,7 +38,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <MarketplaceCountrySignal />
       <MarketplaceCountryPicker />
       <MarketplaceLiveWorkers />
-      <MarketplaceDemoContent />
+      {/* MarketplaceDemoContent intentionally isolated while diagnosing the freeze. */}
       <MarketplaceDemoCurrency />
       <MarketplaceDemoPhotoFix />
       <MarketplaceDemoMapBridge />
