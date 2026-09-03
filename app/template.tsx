3651f4e4e3460a8fecb5d68bf97cc850043dd674
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-09-03 09:58:36 +0300

    Complete original map restoration

--- app/template.tsx
+++ app/template.tsx
@@ -1,3 +1,4 @@
+import JobCardProfilePhotos from "./JobCardProfilePhotos";
 import MobileMarketplaceHeader from "./MobileMarketplaceHeader";
 import MobileLoginAuthFix from "./MobileLoginAuthFix";
 import HideInactiveSocialLogin from "./HideInactiveSocialLogin";
@@ -8,22 +9,25 @@ import LoginViewportFit from "./LoginViewportFit";
 import GlobalMarketplaceLocation from "./GlobalMarketplaceLocation";
 import MarketplaceCountrySignal from "./MarketplaceCountrySignal";
 import MarketplaceCountryPicker from "./MarketplaceCountryPicker";
+import MarketplaceLiveWorkers from "./MarketplaceLiveWorkers";
+import MarketplaceDemoContent from "./MarketplaceDemoContent";
+import MarketplaceDemoMapBridge from "./MarketplaceDemoMapBridge";
+import MarketplaceDemoCountryGuard from "./MarketplaceDemoCountryGuard";
+import MarketplaceDemoCurrency from "./MarketplaceDemoCurrency";
+import MarketplaceDemoPhotoFix from "./MarketplaceDemoPhotoFix";
+import MarketplaceSouthernDemoContent from "./MarketplaceSouthernDemoContent";
+import MarketplaceNorthernDemoContent from "./MarketplaceNorthernDemoContent";
+import MarketplaceRootCountryMapSync from "./MarketplaceRootCountryMapSync";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <style>{`
-        #map.map,
-        .leaflet-container {
-          min-height: 360px;
-          background: #e7efe9;
-        }
-      `}</style>
-
       {/* Render the AnyDayWork first-paint shield before the legacy marketplace
           markup so the browser never gets a chance to paint the old brand first. */}
       <AnyDayWorkStartupGuard />
       {children}
+      <MarketplaceRootCountryMapSync />
+      <JobCardProfilePhotos />
       <MobileMarketplaceHeader />
       <MobileLoginAuthFix />
       <HideInactiveSocialLogin />
@@ -33,6 +37,14 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <GlobalMarketplaceLocation />
       <MarketplaceCountrySignal />
       <MarketplaceCountryPicker />
+      <MarketplaceLiveWorkers />
+      <MarketplaceDemoContent />
+      <MarketplaceDemoCurrency />
+      <MarketplaceDemoPhotoFix />
+      <MarketplaceDemoMapBridge />
+      <MarketplaceDemoCountryGuard />
+      <MarketplaceSouthernDemoContent />
+      <MarketplaceNorthernDemoContent />
     </>
   );
 }
