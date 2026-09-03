6241b1d914ae8c42dbde96eea17cdbfbfb93d751
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-09-03 10:43:48 +0300

    Restore safe UI-only marketplace polish

--- app/template.tsx
+++ app/template.tsx
@@ -1,12 +1,20 @@
+import MobileMarketplaceHeader from "./MobileMarketplaceHeader";
+import HideInactiveSocialLogin from "./HideInactiveSocialLogin";
+import AccountTypographyPolish from "./AccountTypographyPolish";
 import AnyDayWorkBranding from "./AnyDayWorkBranding";
 import AnyDayWorkStartupGuard from "./AnyDayWorkStartupGuard";
+import LoginViewportFit from "./LoginViewportFit";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AnyDayWorkStartupGuard />
       {children}
+      <MobileMarketplaceHeader />
+      <HideInactiveSocialLogin />
+      <AccountTypographyPolish />
       <AnyDayWorkBranding />
+      <LoginViewportFit />
     </>
   );
 }
