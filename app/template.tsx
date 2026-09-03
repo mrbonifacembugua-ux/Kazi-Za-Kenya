064a309d3ef1dca23def94773b963baef77186bf
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-09-03 10:51:31 +0300

    Restore lightweight country map switching

--- app/template.tsx
+++ app/template.tsx
@@ -6,11 +6,14 @@ import LoginViewportFit from "./LoginViewportFit";
 import HideInactiveSocialLogin from "./HideInactiveSocialLogin";
 import MarketplaceCountrySignal from "./MarketplaceCountrySignal";
 import MarketplaceCountryPicker from "./MarketplaceCountryPicker";
+import MarketplaceMapHandleBridge from "./MarketplaceMapHandleBridge";
+import MarketplaceRootCountryMapSync from "./MarketplaceRootCountryMapSync";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AnyDayWorkStartupGuard />
+      <MarketplaceMapHandleBridge />
       {children}
       <MobileMarketplaceHeader />
       <AccountTypographyPolish />
@@ -19,6 +22,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <AnyDayWorkBranding />
       <MarketplaceCountrySignal />
       <MarketplaceCountryPicker />
+      <MarketplaceRootCountryMapSync />
     </>
   );
 }
