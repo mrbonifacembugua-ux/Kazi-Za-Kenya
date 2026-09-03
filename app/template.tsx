d340018f9bc27637711fe73da313b353fd6faa11
mrbonifacembugua-ux <mrbonifacembugua@gmail.com>
2026-09-03 11:28:23 +0300

    Restore demo currency as next controlled batch

--- app/template.tsx
+++ app/template.tsx
@@ -11,6 +11,7 @@ import JobCardProfilePhotos from "./JobCardProfilePhotos";
 import MarketplaceLiveWorkers from "./MarketplaceLiveWorkers";
 import MarketplaceCountryUiSync from "./MarketplaceCountryUiSync";
 import MobileLoginAuthFix from "./MobileLoginAuthFix";
+import MarketplaceDemoCurrency from "./MarketplaceDemoCurrency";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
@@ -29,6 +30,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <JobCardProfilePhotos />
       <MarketplaceLiveWorkers />
       <MobileLoginAuthFix />
+      <MarketplaceDemoCurrency />
     </>
   );
 }
